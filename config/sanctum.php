dba27160694c9d3e72f05ed9be6894026c0a3972
Hambasahaya <zaldyramlan24@gmail.com>
2025-05-04 14:07:13 +0700

    finish bug

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,bemobile-production-82d1.up.railway.app::8080',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
